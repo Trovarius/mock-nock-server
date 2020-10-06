c6843911b94fc60f7bb6f2b8e6707622e6a9712d
Trovarius <tordek.coast@gmail.com>
2020-10-06 21:04:56 +0100

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import proxy from "express-http-proxy";
 import nock from "nock";
+import cors from "cors"
 
 import MockRepository from "./gateways/mock-repository"
 import RegisterMocks from "./use-cases/register-mocks"
 import http, {Server} from "http"
   
 const app = express();
+app.use(cors())
 
 let server : Server;
 
@@ -23,15 +25,15 @@ app.use("/", proxy('mockserver:8888'));
 
 export async function startServer(rootFolder: string = "./test/mocks", port: number = 7777) {
 
-  await registerMocks(rootFolder, `http://mockserver:8888`)
-  
+  await registerMocks(rootFolder)
+
   server = app.listen(port, () => {
     console.log(`\n server running at http://localhost:${port}`);
   });
 }
 
-export async function registerMocks(rootFolder: string = "./mocks", urlBase: string = "http://localhost") {
-  const scope = nock(`http://mockserver:8888`)
+export async function registerMocks(rootFolder: string = "./mocks", urlBase: string = `http://mockserver:8888`) {
+  const scope = nock(urlBase)
   const repo = new MockRepository(rootFolder || "./test/mocks")
   const register = new RegisterMocks(repo, scope);
   await register.register();
