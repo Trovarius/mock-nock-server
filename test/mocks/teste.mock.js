2dc92048771e249b0c20228790879ce1c38c1843
Trovarius <tordek.coast@gmail.com>
2020-10-06 17:34:40 +0100

    feat: add build in package json

--- test/mocks/teste.mock.js
+++ test/mocks/teste.mock.js
@@ -3,11 +3,12 @@ module.exports = (nockScope) => {
     .get("/repos/atom/atom/license")
     .reply(200, {
       license: {
+        teste: () => Date.now(),
         key: "mit",
         name: "MIT License",
         spdx_id: "MIT",
         url: "https://api.github.com/licenses/mit",
-        node_id: "MDc6TGljZW5zZTEzddddddd",
+        node_id: "FRONTEND",
       },
     })
     .persist();
